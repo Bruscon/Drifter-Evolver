6710c38269c520187b8a0765676e12de762f6c54
Bruscon <bruscon@bu.edu>
2020-12-11 12:51:05 -0700

    got multiprocessing standard working satisfactorily. next step is to use it as a template to make MPDrifter

--- mpstandard.py
+++ mpstandard.py
@@ -3,69 +3,91 @@
 Created on Wed Dec  9 11:06:21 2020
 
 @author: Nick Brusco
+
+This works almost perfectly and is to be used as a template for future multiprocessessing.
+The only issue is that you cant delete everything with cleanup() and then 
+make 12 new processes with run(). No idea why it fails. Its not needed for Drifter anyway. 
 """
 import multiprocessing as mp
 import time
+import os, gc, sys
 
 class runner:
-    def __init__(self, name, q, r, e):
+    def __init__(self, name, q, r):
         self.name = name
         self.q = q
         self.r = r
-        self.e = e #event. use as if it were named "is_done_processing"
         
     def f(self):
         while(1):
             fromq = self.q.get()
-            self.e.clear()
-            if fromq == None: #to kill the process
-                break
-            for i in range(10**7): pass #just count really high to spin the wheels
+            print(f"process {os.getpid()} as worker {self.name} recieved {fromq}")
+            
+            if fromq == None: #kill process if we recieve None
+                sys.exit()
+                return None
+            
+            for i in range(10**7): pass #count to ten million to spin the wheels
             self.r.put(fromq*2)
-            self.e.set()
+            print(f"process {os.getpid()} as worker {self.name} finished")
+            
+            
+def cleanup():
+    #not sure how much of this is really necessary but i REALLY wanted
+    #to make sure these processes were dead
+    
+    while q.empty(): #keep adding None's to the queue until there are no more processes to eat them
+        q.put(None)
+        time.sleep(.01)
+    
+    global processes, runners
+    
+    for process in processes:
+        process.kill()
+        process.terminate()
+        process.join(0)
+        
+    gc.collect()
+    
+
+def test(num):
+    '''argument is how many times to count to ten million'''
+    for j in range(num):
+        q.put(j)
+    
+    _start = time.time()
+    
+    summ=0
+    for i in range(num):
+        summ +=r.get()
+    print(f'checksum = {summ}')
+    print(f'took {round(time.time()-_start,2)} seconds')
+    
+    return summ
+
+
+def run(num_processes):
+    global q,r 
+    global processes, runners
+    
+    for i in range(num_processes):
+        runners.append( runner(i, q, r ))
+        processes.append( mp.Process(target=runners[-1].f, args=() ) )
+        processes[-1].start()
+    
+    if not test(40) == test(40): print('ALERT! CHECKSUMS NOT EQUAL!'*4)
+    
 
 if __name__ == '__main__':
+    
+    global q,r, processes, runners
+    
+    processes, runners = [], []
     q = mp.Queue() #tasks queue, a genome will go in here
     r = mp.Queue() #results queue, a fitness score will come out of here
-    
-    
-    for num_processes in range( 1, mp.cpu_count()*2 ) : #test to find the number of processes for fastest execution
-        
-        processes = []
-        runners = []
-        events = [] 
-        
-        for i in range(num_processes):
-            e = mp.Event()
-            events.append( e )
-            runners.append( runner(i, q, r, e) )
-            processes.append( mp.Process(target=runners[-1].f, args=() ) )
-            processes[-1].start()
-            '''one slight bug, all these processes still exist even though I kill
-            them later in the program. Open task manager after the program completes
-            to see what i mean'''
-        
-        for j in range(10):
-            q.put(j)
-        
-        _start = time.time()
-        
-        while not q.empty() : pass
-        for event in events:
-            event.set() #blocks (hopefully without polling?) until event is True
-        
-        #print(r.get(), r.get(), r.get(), r.get(), r.get(), r.get(), r.get(), r.get(), r.get(), r.get())
-        print(f'took {round(time.time()-_start,2)} seconds with {num_processes} processes')
-        
-        #kill all the processes
-        for j in range(num_processes):
-            q.put(None)
-        
-        
-        for process in processes:
-            process.terminate()
-            process.join(2)
-            del process
-           
-            
-    breakpoint()
+
+    run(mp.cpu_count())
+    test(500)
+
+
+
